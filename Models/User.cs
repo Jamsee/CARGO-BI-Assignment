a34add30293cef6d04310cb92b437f5d8ab48ba4
Jamie <Jamie@JAMIE>
2024-03-03 20:06:36 +0100

    Add project files.

---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace CARGO_B_IAssignment.Models;
+
+public class User
+{
+    public string UserID { get; set; }
+    public string UserName { get; set; }
+
+    public string Email { get; set; }
+    public string Address { get; set; }
+
+}
+
